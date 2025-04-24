5a76acd32057e9d7462cfd2bf3f63ce4b0a7ffd7
Lmacconnell2 <mac23068@byui.edu>
2025-04-24 14:58:51 -0600

    added new function things

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        // int x = 5;
+        // Console.WriteLine(x);
+        Console.Write("What is your favorite color? ");
+        string color = Console.ReadLine();
+        Console.WriteLine(color);
+
+        if (color == "Blue")
+        {
+            Console.WriteLine($"{color} is my favorite color too!");
+        }
+        else {
+            Console.WriteLine($"{color} is a disgusting color.");
+        }
     }
 }
